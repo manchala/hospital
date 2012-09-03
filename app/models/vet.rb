fb87b9c92b1239891c79a86072aac5d663f9190b
manchala <vinita_manchala@hotmail.com>
2012-09-02 21:47:31 -0700

    New composite appointment  added

--- app/models/vet.rb
+++ app/models/vet.rb
@@ -5,8 +5,8 @@ class Vet < ActiveRecord::Base
   has_many :pets, :through => :appointments
   VALID_ZIP = 	/^\d{5}$/
   
-  validates :name, presence: true, length: {maximum :35}
-  validates :zip, format :{with :VALID_ZIP}
-  validates :num_years, numericality =>{:greater_than =>0, :less_than_or_equal_to => :100}
+   validates :name,  presence: true, length: { maximum: 50 }
+  validates :zip, presence: true, format: { with: VALID_ZIP }
+  
   
 end
