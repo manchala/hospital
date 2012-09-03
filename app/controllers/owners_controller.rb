5f92c5f403a81ea391b50af4e411af81de3295f9
manchala <vinita_manchala@hotmail.com>
2012-09-02 21:39:40 -0700

    New composite primary  added

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,5 +1,6 @@
 class OwnersController < ApplicationController
   def show
       @pet = Pet.find(params[:id])
+      @item = @pet.appointments.find(params[:id])
   end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,18 +3,16 @@ class Appointment < ActiveRecord::Base
   
   belongs_to :Vet
   belongs_to :Pet
+  set_primary_keys :vet_id, :pet_id
   belongs_to :Reminder 
   
-  validates :name, presence: true, length :{ maximum :35 }
-  validates :type_of_pet, in => ['dog', 'cat', 'bird']
+   validates :name,  presence: true, length: { maximum: 35 }
+  
   validates :pet_id, presence: true
   validates :owner_id, presence: true
   validates :visit_reason, presence: true
   validates :date_of_visit, presence: true
-  validate : no_past_date
   
-  def no_past_date
-    if self.date_of_visit < Date.today
-      errors.add( :date, 'notinpast')
-    end
+
+  
 end
