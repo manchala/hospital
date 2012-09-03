b0f4b5c1a95031ff5a266318e987b4b5a810c152
manchala <vinita_manchala@hotmail.com>
2012-09-02 21:41:15 -0700

    New composite primary  added

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,12 +7,16 @@ class Appointment < ActiveRecord::Base
   belongs_to :Reminder 
   
    validates :name,  presence: true, length: { maximum: 35 }
-  
+  validate : no_past_date
   validates :pet_id, presence: true
   validates :owner_id, presence: true
   validates :visit_reason, presence: true
   validates :date_of_visit, presence: true
   
-
+ def no_past_date
+    if self.date_of_visit < Date.today
+      errors.add( :date_of_visit, 'notinpast')
+    end
+  end
   
 end
